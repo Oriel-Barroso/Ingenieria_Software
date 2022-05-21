acd644369c7a1d81634c2a13746d7c9899d23635
Oriel-Barroso <oriel_gold97@outlook.com>
2022-05-21 19:07:53 -0300

    segundo commit

--- red_social/app1/urls.py
+++ red_social/app1/urls.py
@@ -4,4 +4,5 @@ from app1 import views
 
 urlpatterns = [
     path('registro/', views.registro),
+    path('get_user/', views.get_user),
 ]
--- red_social/app1/views.py
+++ red_social/app1/views.py
@@ -1,8 +1,9 @@
 import email
-from django.http import HttpResponse
+from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
 from django.contrib.auth.models import User
 from django.views.decorators.csrf import csrf_exempt
+from more_itertools import first
 from rest_framework.renderers import JSONRenderer
 from rest_framework.parsers import JSONParser
 from rest_framework.decorators import api_view
@@ -12,16 +13,20 @@ from rest_framework.decorators import api_view
 @api_view(('POST',))
 def registro(req):
     data = JSONParser().parse(req)
-    # print(data['firstname'])
-    # print(data['lastname'])
-    # print(data['username'])
-    # print(data['email'])
-    # print(data['password'])
     user = User.objects.create_user(first_name=data['firstname'],
-                               last_name=data['lastname'],
-                               email=data['email'],
-                               username=data['username'],
-                               password=data['password']
-                              )
+                                    last_name=data['lastname'],
+                                    email=data['email'],
+                                    username=data['username'],
+                                    password=data['password']
+                                   )
     user.save()
+    return HttpResponse(user, status=200)
+
+@csrf_exempt 
+@api_view(('POST',))
+def get_user(req):
+    data = JSONParser().parse(req)
+    nombre = data['username']
+    user = User.objects.filter(username=nombre).values()
+    print(user, 'PRINT DEL USUARIO')
     return HttpResponse(user, status=200)
