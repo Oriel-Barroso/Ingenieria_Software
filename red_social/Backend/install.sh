d0eb9ed489718d86b51ce99464c930d2e05929a5
Martin-Ruggeri-Bio <martinruggeri18@gamil.com>
2022-05-28 15:25:45 -0300

    mis modificaciones en una rama propia

--- red_social/Backend/install.sh
+++ red_social/Backend/install.sh
@@ -4,4 +4,4 @@ pip install --upgrade pip
 pip3 install -r requirements.txt
 python manage.py makemigrations
 python manage.py migrate
-python manage.py createsuperuser
+#python manage.py createsuperuser
